ed87375ac0e1bd640ea5e7b75370e5bb1ddcc13e
IvyLee-wu <45351785+IvyLee-wu@users.noreply.github.com>
2019-05-14 12:30:20 +0800

    update

--- src/components/WatchFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <footer class="bg-dark py-4 d-flex justify-content-center align-items-center bg-dark">
-      <p class="text-white text-center m-0">© Copyright 2019 <br> 僅提供練習用 無商業用途</p>
-    </footer>
-  </div>
-</template>
