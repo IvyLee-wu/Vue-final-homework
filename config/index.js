47a174e0cf8027e71dbb9df1e5b0b4a2af426e05
IvyLee-wu <45351785+IvyLee-wu@users.noreply.github.com>
2019-05-16 15:41:22 +0800

    update

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Vue-final-homework/',
+    assetsPublicPath: '/Vue-final-homework/dist/',
 
     /**
      * Source Maps
